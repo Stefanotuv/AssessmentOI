4b57b7956e70a8f7bae3cfda1168d5362de74ddf
stefanotuv <ste.tuveri@gmail.com>
2023-06-13 23:29:54 +0100

    ready to be sent to production on

--- AssessmentOI/AssessmentOI/settings.py
+++ AssessmentOI/AssessmentOI/settings.py
@@ -36,6 +36,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Application definition
 
@@ -212,6 +213,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
 
 
 # Static files (CSS, JavaScript, Images)
